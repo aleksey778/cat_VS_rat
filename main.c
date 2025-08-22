ebf4df042851898924a86776c208b5169ba77b85
alex <alexlinux777@gmail.com>
2025-08-22 10:19:59 +0300

    add basic mechanic for cat and rat + simple refactor in code

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 #include <ncurses.h>
 
 
@@ -20,8 +21,11 @@ void drawing_map(char **map, int rows, int cols)
             map[y][x] = '#';
         }
     //пустые места
-    for (int y = 5; y < rows-10; y++)
-        for (int x = 5; x < cols-10; x++) {
+    int  r_x = (rand() % (cols / 3)) + 1; //[1; (cols / 3)]
+    int r_y = (rand() % (rows / 3)) + 1; //[1; (rows / 3)]
+    
+    for (int y = r_y; y < rows-r_y; y++)
+        for (int x = r_x; x < cols-r_x; x++) {
             mvaddch(y, x, ' ');
             map[y][x] = ' ';
         }
@@ -51,7 +55,7 @@ const int *my_key_input, char my_role)
     else if (*my_key_input == KEY_RIGHT)
         (*px)++;
     
-    
+ 
     //если шаг на стену/границу - возвращение персонажа на исходную позицию
     if (map[*py][*px] == '%' || (my_role == 'c' && map[*py][*px] == '#')) {
         *px = *last_x;
@@ -66,15 +70,69 @@ const int *my_key_input, char my_role)
 }
 
 
-void move_enemy(char **map, int *last_x, int *last_y, int *ex, int *ey, char enemy_role)
+void move_enemy(char **map, int *last_x, int *last_y, int *ex, int *ey,
+int px, int py, char enemy_role)
 {
     *last_x = *ex, *last_y = *ey;
+    
+    // для кота
+    if (enemy_role == 'c') {
+        int r = rand() % 2; //[0; 1]
+        //примечение: если равны по х, то не трогать х, если ==у, то у
+        //X преследование
+        if ((r || *ey == py) && *ex != px) {
+            if ((*ex <= px) && (map[*ey][(*ex)+1] != '%' && map[*ey][(*ex)+1] != '#'))
+                (*ex)++;
+            else if ((*ex >= px) && (map[*ey][(*ex)-1] != '%' && map[*ey][(*ex)-1] != '#'))
+                (*ex)--;
+        }
+        //Y преследование
+        else {
+            if ((*ey <= py) && (map[(*ey)+1][*ex] != '%' && map[(*ey)+1][*ex] != '#'))
+                (*ey)++;
+            else if ((*ey >= py) && (map[(*ey)-1][*ex] != '%' && map[(*ey)-1][*ex] != '#'))
+                (*ey)--;
+        }
+    }
+    //для крысы
+    else if (enemy_role == 'r') {
+        int r = rand() % 2; //[0; 1]
+        //X побег
+        if (r && *ey != py) {
+            if ((*ex <= px) && (map[*ey][(*ex)-1] != '%' && map[*ey][(*ex)-1] != 'c'))
+                (*ex)--;
+            else if ((*ex >= px) && (map[*ey][(*ex)+1] != '%' && map[*ey][(*ex)+1] != 'c'))
+                (*ex)++;
+        }
+        //Y побег
+        else {
+            if ((*ey <= py) && (map[(*ey)-1][*ex] != '%' && map[(*ey)-1][*ex] != 'c'))
+                (*ey)--;
+            else if ((*ey >= py) && (map[(*ey)+1][*ex] != '%' && map[(*ey)+1][*ex] != 'c'))
+                (*ey)++;
+        }
+    }
     
     mvaddch(*last_y, *last_x, ' '); //clear last position
     map[*last_y][*last_x] = ' ';
     
     mvaddch(*ey, *ex, enemy_role); //print new position
     map[*ey][*ex] = enemy_role;    
+}
+
+
+bool fight_if_collision(char **map, int px, int py, int ex, int ey,
+int rows, int cols, char my_role)
+{
+    bool collision = (px == ex) && (py == ey);
+    if (collision) {
+        const char *end_text = (my_role == 'c') ? "win" : "lose";
+        clear_map(map, rows, cols);
+        mvprintw(rows/2, cols/2, "You %s\n", end_text);
+        getch();
+    }
+    
+    return collision;
 }
 
 
@@ -142,17 +200,15 @@ int main()
     
     //Главный цикл
     do {
-        move_me(map, &last_x, &last_y, &px, &py, &c, my_role);
-        move_enemy(map, &last_x_enemy, &last_y_enemy, &ex, &ey, enemy_role);
         
-        //столкновение
-        if (px == ex && py == ey) {
-            const char *end_text = (my_role == 'c') ? "win" : "lose";
-            clear_map(map, rows, cols);
-            mvprintw(rows/2, cols/2, "You %s\n", end_text);
-            getch();
+        move_me(map, &last_x, &last_y, &px, &py, &c, my_role);
+        if (fight_if_collision(map, px, py, ex, ey, rows, cols, my_role) == true)
             break;
-        }
+        
+        move_enemy(map, &last_x_enemy, &last_y_enemy, &ex, &ey,
+        px, py, enemy_role);
+        if (fight_if_collision(map, px, py, ex, ey, rows, cols, my_role) == true)
+            break;
         
     } while ((c = getch()) && c != 'q' && c != 27); //27 - ESC
     
